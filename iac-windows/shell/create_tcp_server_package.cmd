ee66ac582db032481b8dcc34d1620912c031f53f
Mauricio Magnani <msmagnanijr@gmail.com>
2021-07-20 22:05:34 -0300

    refactor windows example

--- iac-windows/shell/create_tcp_server_package.cmd
+++ iac-windows/shell/create_tcp_server_package.cmd
@@ -3,3 +3,4 @@ REM https://datatofish.com/executable-pyinstaller/
 pip install pyinstaller
 
 pyinstaller --onefile --windowed C:\tmp\tcp-server.py
+
---
+++ iac-windows/shell/install_tcp_server.cmd
@@ -0,0 +1,2 @@
+REM TODO NOT WORKING
+setup.exe C:\Windows\system32\tcp-server.exe
